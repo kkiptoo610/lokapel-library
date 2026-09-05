3912d6926ba6d27573255c774ceffdb112a2b732
Kiptoo Kelvin <kkiptoo610@gmail.com>
2026-09-05 09:18:32 +0300

    Fix librarian password

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,19 +14,16 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::updateOrCreate(
-
             [
                 'email' => 'library@lokapelsenior.local',
             ],
-
             [
                 'name' => 'Lokapel Senior',
 
                 'email' => 'library@lokapelsenior.local',
 
-                'password' => Hash::make('1234'),
+                'password' => Hash::make('123456'),
             ]
-
         );
     }
 }
